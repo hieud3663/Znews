329389b81343c2f3d95f570b855bef1e23e9740e
hieud3663 <hieud3663@gmail.com>
2024-11-07 23:38:24 +0700

    update

--- JS/more_header.js
+++ JS/more_header.js
@@ -44,6 +44,27 @@ function searchMenu() {
 document.querySelectorAll('.article-thumbnail a').forEach(link => {
   link.addEventListener('click', function(event) {
     event.preventDefault();
-    window.location.href = 'trang-bao.html?src=' + encodeURIComponent(this.href);
+    window.open('trang-bao.html?src=' + encodeURIComponent(this.href), '_blank');
+  });
+});
+
+
+document.querySelectorAll('.article-title a').forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    window.open('trang-bao.html?src=' + encodeURIComponent(this.href), '_blank');
+  });
+});
+
+document.querySelectorAll('.article a').forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    window.open('trang-bao.html?src=' + encodeURIComponent(this.href), '_blank');
+  });
+});
+document.querySelectorAll('.info a').forEach(link => {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    window.open('trang-bao.html?src=' + encodeURIComponent(this.href), '_blank');
   });
 });
